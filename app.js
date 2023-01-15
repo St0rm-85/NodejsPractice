7a85b3f238fcdecd981841e15d98364a1e7d030e
St0rm85 <hung.do4@tiki.vn>
2023-01-15 15:53:51 +0700

    update

---
+++ app.js
@@ -0,0 +1,9 @@
+//const { create } = require('domain');
+const http = require('http');
+const routes = require('./routes');
+
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
+
+server.listen(3000);
---
+++ test1.js
@@ -0,0 +1,62 @@
+// // var Name = "hung";
+// // let age = 11;
+// // var hasHobby = "gym";
+
+// // const summarizeUser = (userName, userAge, userHobby) =>
+// // {
+// //     return (
+// //         'My name is ' + userName + ', age is ' + userAge + ' and my hobby is ' + userHobby
+// //     );
+// // }
+
+// // const add = (a,b) => a+b;
+
+
+
+// // console.log(add(1,2))
+// // console.log(summarizeUser(Name, age, hasHobby));
+
+// const person = {
+//     name: 'Hung',
+//     age: 20,
+//     greet () {
+//         console.log('Hello, I am ' + this.name);
+//     } 
+// }
+
+// const printName = ({ name }) => {
+//     console.log(name);
+// }
+  
+// printName(person);
+
+// const {name, age} = person;
+// console.log(name, age);
+
+// // const hobbies = ['gym', 'football'];
+// // for (let hobby of hobbies){
+// //     console.log(hobby);
+// // }
+// // console.log(hobbies);
+// // console.log(hobbies.map(hobby => 'Hobby: ' + hobby))
+
+// // hobbies.push('hacking');
+// // console.log(hobbies);
+
+// // const copiedArray = hobbies;
+// // console.log(copiedArray); 
+
+// // const toArray = (...args) => {
+// //     return args;
+// // }
+
+// // console.log(toArray(1,2,3,4));
+
+
+setTimeout(() => {
+    console.log('Time is done!');
+}, 0);
+
+console.log('hello');
+console.log('hi');
+
